93204f4f28b9a48835eba09e35ede770c60b8480
s.budnikov <sergei.budnikov@smartseeds.ru>
2024-06-05 13:10:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("DateTime Formats");
+Console.WriteLine(DateTime.Now.ToString("%K"));
+// Displays -07:00
+Console.WriteLine(DateTime.UtcNow.ToString("O"));
+// Displays Z
+Console.WriteLine("'{0}'",
+                  DateTime.SpecifyKind(DateTime.Now,
+                       DateTimeKind.Utc).ToString("O"));
+// Displays ''
+Console.WriteLine("Offset Formats");
+Console.WriteLine(DateTimeOffset.Now.ToString("O"));
+Console.WriteLine(DateTimeOffset.Now.UtcDateTime.ToString("O"));
+Console.WriteLine(DateTimeOffset.Now.DateTime.ToString("O"));
+// Displays -07:00
+Console.WriteLine(DateTimeOffset.UtcNow.ToString("%K"));
+// Displays +00:00
+Console.WriteLine(new DateTimeOffset(2008, 5, 1, 6, 30, 0,
+                      new TimeSpan(5, 0, 0)).ToString("%K"));
+// Displays +05:00
+Console.WriteLine("Hello, World!");
